653ee1cb5dcf51e9f22156af6d36c654cfe53fad
alexmsantos3 <alexm.santos@hotmail.com>
2020-07-29 10:08:31 -0300

    Carousel Slider incluido

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import Menu from './components/Menu';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+//Desafio master blaster na descrição(pagina legal, com game talvez) 
+const Pagina404 = () => (<div><Menu />Página 404</div>);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
+    </Switch>
+    
+    {/* <Switch>
+      <Route path="cadCat" component={CadastroCategoria} />
+    </Switch> */}
+
+  </BrowserRouter>,
+  
   document.getElementById('root')
 );
